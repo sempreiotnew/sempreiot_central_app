93aa81a5d09a3f3d1ab9067e4db692c031e40148
tallesaugusto <tallesaugusto1989@gmail.com>
2026-06-05 13:35:11 -0300

    update

--- lib/core/connectivity/connectivity_provider.dart
+++ lib/core/connectivity/connectivity_provider.dart
@@ -1,8 +1,11 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final connectivityProvider = StreamProvider<bool>((ref) {
-  return Connectivity().onConnectivityChanged.map(
-        (results) => results.any((r) => r != ConnectivityResult.none),
-      );
+final connectivityProvider = StreamProvider<bool>((ref) async* {
+  final connectivity = Connectivity();
+  final initial = await connectivity.checkConnectivity();
+  yield initial.any((r) => r != ConnectivityResult.none);
+  yield* connectivity.onConnectivityChanged.map(
+    (results) => results.any((r) => r != ConnectivityResult.none),
+  );
 });
